0154b75da667015fc56d26dfeb4a6ea5cabb15b6
Mr8649 <864910574@qq.com>
2019-03-31 11:27:09 +0800

    2019/03/31  11:26图片上传已经成功了

--- ly-upload/src/main/java/com/leyou/upload/service/UploadService/UploadService.java
+++ ly-upload/src/main/java/com/leyou/upload/service/UploadService/UploadService.java
@@ -2,75 +2,56 @@ package com.leyou.upload.service.UploadService;
 
 import com.github.tobato.fastdfs.domain.StorePath;
 import com.github.tobato.fastdfs.service.FastFileStorageClient;
-import com.leyou.common.enums.ExceptionEnum;
-import com.leyou.common.exception.LyException;
-import com.leyou.upload.config.UploadProperties;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang.StringUtils;
+import com.leyou.upload.web.UploadController;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 @Service
-@Slf4j
-@EnableConfigurationProperties(UploadProperties.class)
-//@Resource
-
 public class UploadService {
-    @Autowired
-    private FastFileStorageClient storageClient;
-    @Autowired
-    private UploadProperties prop;
+
+    private static final Logger logger = LoggerFactory.getLogger(UploadController.class);
+
     // 支持的文件类型
-    //private static final List<String> ALLOW_TYPES = Arrays.asList("image/png", "image/jpeg", "image/bmp", "image/jpg");
+    private static final List<String> suffixes = Arrays.asList("image/png", "image/jpeg");
 
-    public String uploadImage(MultipartFile file) {
+    @Autowired
+    FastFileStorageClient storageClient;
 
-        //this.getClass().getClassLoader().getResource("").getFile();
+    public String upload(MultipartFile file) {
         try {
-            /**
-             * 其实图片上传就是基本上就是两个流程，一个是准备路径，一个是保存
-             * ====但是要完善一下，图片来了以后我需要进行在上传之前做校验(一般校验后缀名)
-             */
-            //1：校验
-            //1）校验文件类型====怎么去定义一个文件的类型？===》放到配置文件中
-            String contentType = file.getContentType();
-            //if (!ALLOW_TYPES.contains(contentType)) {
-            if (!prop.getAllowTypes().contains(contentType)) {
-                throw new LyException(ExceptionEnum.INVALID_FILE_TYPE);
+            // 1、图片信息校验
+            // 1)校验文件类型
+            String type = file.getContentType();
+            if (!suffixes.contains(type)) {
+                logger.info("上传失败，文件类型不匹配：{}", type);
+                return null;
             }
-            //2）当文件类型为空的时候，或者出于安全考虑，还需要对文件的内容进行校验
+            // 2)校验图片内容
             BufferedImage image = ImageIO.read(file.getInputStream());
-            //下面image==null是有一种校验方法，还有有一种是通过校验文件的长宽高等属性来判断的
             if (image == null) {
-                throw new LyException(ExceptionEnum.INVALID_FILE_TYPE);
+                logger.info("上传失败，文件内容不符合要求");
+                return null;
             }
 
-            //准备目标路径
-            //File dest = new File("C:\\Users\\njtech\\Desktop\\upload", file.getOriginalFilename());//这个是保存在本地，当使用FastDFS后就需要改变了
-            //保存文件到本地
-            //file.transferTo(dest);
-
-            //上传到FastDFS
-            //String extension=file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf(".")+1);
-            //上一句代码完善如下：
+            // 2、将图片上传到FastDFS
+            // 2.1、获取文件后缀名
             String extension = StringUtils.substringAfterLast(file.getOriginalFilename(), ".");
-            StorePath storePath = storageClient.uploadFile(file.getInputStream(), file.getSize(), extension, null);
-
-            //返回路径(返回一个可以访问的路径，一般用域名访问呢)
-            //return "http://image.leyou.com/" +storePath;
-            return prop.getBaseUrl() +storePath;
-            //return "http://image.leyou.com/" + file.getOriginalFilename();
-        } catch (IOException e) {
-            //上传失败
-            log.error("[文件上传]上传文件失败", e);//上传文件失败并且打出日志
-            throw new LyException(ExceptionEnum.UPLOAD_FILE_ERROR);
+            // 2.2、上传
+            StorePath storePath = this.storageClient.uploadFile(
+                    file.getInputStream(), file.getSize(), extension, null);
+            // 2.3、返回完整路径
+            return "http://image.leyou.com/" + storePath.getFullPath();
+        } catch (Exception e) {
+            return null;
         }
     }
 }
-
--- ly-upload/src/main/java/com/leyou/upload/web/UploadController.java
+++ ly-upload/src/main/java/com/leyou/upload/web/UploadController.java
@@ -1,7 +1,9 @@
 package com.leyou.upload.web;
 
 import com.leyou.upload.service.UploadService.UploadService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,25 +15,22 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("upload")
 public class UploadController {
 
-    /**
-     * 功能描述: 上传图片的功能
-     * 〈〉
-     *
-     * @Author:小艺
-     * @param
-     * @return:
-     * @since: 1.0.0
-     * @Date: 2019/3/29 22:13
-     */
-
     @Autowired
- private  UploadService uploadService;
+    private UploadService uploadService;
+
+    /**
+     * 上传图片功能
+     * @param file
+     * @return
+     */
     @PostMapping("image")
-    public ResponseEntity<String> uploadImage(@RequestParam("file") MultipartFile file){
-        //String upload = uploadService.upload(file);
-        //return ResponseEntity.ok(upload);
-        String url = uploadService.uploadImage(file);
+    public ResponseEntity<String> uploadImage(@RequestParam("file") MultipartFile file) {
+        String url = this.uploadService.upload(file);
+        if (StringUtils.isBlank(url)) {
+            // url为空，证明上传失败
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        // 返回200，并且携带url路径
         return ResponseEntity.ok(url);
-        //简写的形式如下：===return ResponseEntity.ok(uploadService.uploadImage(file));
     }
 }
